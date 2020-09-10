710732190962f8b9c314846b0e4a7d881bd6e5f6
Tapasya Khatri <takhat014@gmail.com>
2020-09-09 19:28:25 -0700

    Initial commit

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            var notes = new List<Note>();
+
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "*.notes.txt");
+            foreach (var filename in files)
+            {
+                var note = new Note
+                {
+                    Text = File.ReadAllText(filename),
+                    Filename = filename,
+                    Date = File.GetCreationTime(filename)
+                };
+                notes.Add(note);
+            }
+
+            listView.ItemsSource = notes.OrderBy(n => n.Date).ToList();
+        }
+
+        private async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        private async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushModalAsync(new NoteEntryPage
+                {
+                    BindingContext = (Note)e.SelectedItem
+                });
+            }
+        }
+    }
+}
---
+++ Notes/Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if(string.IsNullOrWhiteSpace(note.Filename))
+            {
+                //create and save
+                var filename = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, editor.Text);
+            }
+            else
+            {
+                //Update
+                File.WriteAllText(note.Filename, editor.Text);
+            }
+
+            await Navigation.PopModalAsync();
+        }
+
+        private async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+            await Navigation.PopModalAsync();
+        }
+    }
+}
